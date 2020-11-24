6bb555cd84b3cc0cb3e9e60f8debe6f01d0c0fc2
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2020-11-24 16:54:56 +0200

    Add social links and email to the static model

--- application/models/Static_model.php
+++ application/models/Static_model.php
@@ -8,6 +8,9 @@ class Static_model extends CI_Model {
 		$data['tagline'] = "A simple blog application made with Codeigniter 3";
 		$data['company_name'] = "My Company";
 		$data['company_email'] = "company@domain.com";
+		$data['twitter'] = "https://twitter.com/";
+		$data['facebook'] = "https://facebook.com/";
+		$data['instagram'] = "https://instagram.com/";
 		$data['is_frontend'] = false;
 		$data['is_ckeditor'] = false;
 		$data['is_cookieconsent'] = true;
