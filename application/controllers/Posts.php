2e25a046b2d9070375a649570540ef93c0cb17da
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-03-14 18:10:11 +0200

    Delete extra line

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -3,7 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Posts extends CI_Controller
 {
-    
     public function __construct()
     {
         parent::__construct();
