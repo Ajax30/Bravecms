eb96c1fdfd7bb09a95e7acad70035d789505fb50
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2021-10-14 13:58:04 +0300

    format code fo load more script

--- themes/clean-blog/assets/js/ajax-pager.js
+++ themes/clean-blog/assets/js/ajax-pager.js
@@ -1,40 +1,40 @@
 (function($) {
 
-	var currentPage = 2;
+  var currentPage = 2;
   var posts = null;
 
-	$('.pagination').hide();
+  $('.pagination').hide();
 
-	$(window).scroll(function() {
-			if ($(window).scrollTop() >= $(document).height() - $(window).height() - 25) {
-					loadMore();
-			}
-	});
-
-	function loadMore() {
-		$.ajax({
-      url: baseUrl + '?page=' + currentPage,
-      type: 'GET',
-      beforeSend: function() {
-        if (typeof posts != 'undefined') {
-          $('.loader').show();
-        }
+  $(window).scroll(function() {
+      if ($(window).scrollTop() >= $(document).height() - $(window).height() - 25) {
+          loadMore();
       }
-    })
-    .done(function(data) {
-      $('.loader').hide();
+  });
 
-      posts = $(data).find('#postsContainer').html();
+  function loadMore() {
+      $.ajax({
+              url: baseUrl + '?page=' + currentPage,
+              type: 'GET',
+              beforeSend: function() {
+                  if (typeof posts != 'undefined') {
+                      $('.loader').show();
+                  }
+              }
+          })
+          .done(function(data) {
+              $('.loader').hide();
 
-      // If there are no more posts, hide loader
-      //  Otherwise, load more posts
-      if (typeof posts == 'undefined') {
-        $('.loader').hide();
-      } else {
-        $('#postsContainer').append(posts);
-        currentPage = currentPage + 1;
-      }
-    });
-	}
+              posts = $(data).find('#postsContainer').html();
+
+              // If there are no more posts, hide loader
+              //  Otherwise, load more posts
+              if (typeof posts == 'undefined') {
+                  $('.loader').hide();
+              } else {
+                  $('#postsContainer').append(posts);
+                  currentPage = currentPage + 1;
+              }
+          });
+  }
 
 })(jQuery);
